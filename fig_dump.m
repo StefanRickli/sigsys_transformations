0a9575c1fdb27419b8f568b813f6589b2154eabd
Stefan Rickli <stefanrickli@gmx.ch>
2018-11-01 10:31:52 +0100

    Add fig_dump.m
    This script simply dumps the figures to PNG files with a resolution
    of 300dpi.

---
+++ fig_dump.m
@@ -0,0 +1,10 @@
+
+print('-r300', f1, '1-1.png', '-dpng');
+print('-r300', f2, '1-2.png', '-dpng');
+print('-r300', f3, '1-3.png', '-dpng');
+print('-r300', f4, '1-4.png', '-dpng');
+print('-r300', f5, '2-1.png', '-dpng');
+print('-r300', f6, '2-2.png', '-dpng');
+print('-r300', f7, '2-3.png', '-dpng');
+print('-r300', f8, '2-4.png', '-dpng');
+
